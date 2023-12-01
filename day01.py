c85cc30c4f03d6649b7655206af7f51095c74e23
karelinas <45606302+karelinas@users.noreply.github.com>
2023-12-02 05:33:14 +0900

    Refactor solution 1

--- day01.py
+++ day01.py
@@ -1,25 +1,55 @@
 from sys import stdin
-from typing import Iterable
-
-DIGIT_NAMES = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-}
-DIGITS = {str(num): num for num in range(1, 10)}
-DIGITS_AND_NAMES = {**DIGITS, **DIGIT_NAMES}
+from typing import Iterable, NewType, TypeVar, Type
 
 
 def main() -> None:
-    calibration_document: str = stdin.read()
-    print("Part 1:", calibration_value(calibration_document))
-    print("Part 2:", corrected_calibration_value(calibration_document))
+    data: str = stdin.read()
+    calibration_document = CalibrationDocument.from_string(data)
+    correct_calibration_document = CorrectedCalibrationDocument.from_string(data)
+    print("Part 1:", calibration_document.calibration_value())
+    print("Part 2:", correct_calibration_document.calibration_value())
+
+
+DigitTranslationTable = dict[str, int]
+CalibrationLine = NewType("CalibrationLine", list[int])
+T = TypeVar("T", bound="CalibrationDocument")
+
+
+class CalibrationDocument:
+    translation_table: DigitTranslationTable = {str(num): num for num in range(1, 10)}
+
+    def __init__(self, lines: list[CalibrationLine]):
+        self.lines: list[CalibrationLine] = lines
+
+    @classmethod
+    def from_string(cls: Type[T], data: str) -> T:
+        return cls(
+            list(
+                CalibrationLine(
+                    list(digit_generator(line, table=cls.translation_table))
+                )
+                for line in data.split("\n")
+                if line
+            )
+        )
+
+    def calibration_value(self) -> int:
+        return sum(10 * line[0] + line[-1] for line in self.lines)
+
+
+class CorrectedCalibrationDocument(CalibrationDocument):
+    translation_table: DigitTranslationTable = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        **CalibrationDocument.translation_table,
+    }
 
 
 def digit_generator(calibration_line: str, *, table: dict) -> Iterable[int]:
@@ -30,23 +60,6 @@ def digit_generator(calibration_line: str, *, table: dict) -> Iterable[int]:
                 break
 
         calibration_line = calibration_line[1:]
-
-
-def line_value(calibration_digits: list[int]) -> int:
-    return 10 * calibration_digits[0] + calibration_digits[-1]
-
-
-def calibration_value(calibration_document: str, *, table=None) -> int:
-    table = table or DIGITS
-    return sum(
-        line_value(list(digit_generator(line, table=table)))
-        for line in calibration_document.split("\n")
-        if line
-    )
-
-
-def corrected_calibration_value(calibration_document: str) -> int:
-    return calibration_value(calibration_document, table=DIGITS_AND_NAMES)
 
 
 if __name__ == "__main__":
--- test_day01.py
+++ test_day01.py
@@ -1,22 +1,29 @@
 import unittest
 
-from day01 import calibration_value, corrected_calibration_value
+from day01 import CalibrationDocument, CorrectedCalibrationDocument
 
 
 class Day01TestCase(unittest.TestCase):
-    def test_calibration_value_part1_example(self):
+    def test_calibration_value_example_part1(self):
         example_input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n"
-        self.assertEqual(calibration_value(example_input), 142)
+        document = CalibrationDocument.from_string(example_input)
+        self.assertEqual(document.calibration_value(), 142)
 
-    def test_calibration_value_part2_example(self):
+    def test_calibration_value_example_part2(self):
         example_input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n"
-        self.assertEqual(corrected_calibration_value(example_input), 281)
+        document = CorrectedCalibrationDocument.from_string(example_input)
+        self.assertEqual(document.calibration_value(), 281)
 
     def test_overlapping_digit_names(self):
-        self.assertEqual(corrected_calibration_value("2eighthree"), 23)
+        document = CorrectedCalibrationDocument.from_string("2eighthree")
+        self.assertEqual(document.calibration_value(), 23)
 
     def test_real_input(self):
         with open("inputs/day01.txt", "r") as f:
-            calibration_document: str = f.read()
-            self.assertEqual(calibration_value(calibration_document), 55123)
-            self.assertEqual(corrected_calibration_value(calibration_document), 55260)
+            data: str = f.read()
+            document = CalibrationDocument.from_string(data)
+            correct_document = CorrectedCalibrationDocument.from_string(data)
+            with self.subTest(msg="Part 1"):
+                self.assertEqual(document.calibration_value(), 55123)
+            with self.subTest(msg="Part 2"):
+                self.assertEqual(correct_document.calibration_value(), 55260)
