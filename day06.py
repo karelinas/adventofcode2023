f9ac6dac826e2fe1141a0603f878515fa53e046b
karelinas <45606302+karelinas@users.noreply.github.com>
2023-12-06 19:16:45 +0900

    Day 06 optimisation

--- day06.py
+++ day06.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from sys import stdin
-from math import prod
+from math import prod, sqrt, ceil, floor
 import re
 
 
@@ -47,11 +47,44 @@ def distance_traveled(v: int, t: int) -> int:
 
 
 def count_winning_strategies(race: Race) -> int:
-    return sum(
-        1
-        for a in range(race.max_time)
-        if distance_traveled(a, race.max_time - a) > race.record
-    )
+    # Travel time:
+    #
+    #   t = r - b            (1)
+    #
+    # Where: t = travel time
+    #        r = race time
+    #        b = button press duration
+    #
+    # Travel distance:
+    #
+    #   s = b * t            (2)
+    #
+    # Where: s = distance traveled (set to race.record + 1)
+    #
+    # Substitute (1) in (2):
+    #
+    #   s = b*(r - b)
+    #   s = b*r - b**2
+    #   b**2 - r*b + s = 0   (3)
+    #
+    # Solve (3) for b using the quadratic equation:
+    #
+    #   b1 = (-r - sqrt(r**2 - 4 * s)) / 2
+    #   b2 = (-r + sqrt(r**2 - 4 * s)) / 2
+    #
+    # Where b1 is the minimum button press duration to win the race, and b2 is the
+    # maximum button press duration to win the race.
+    #
+    # The number of button press durations that win is the number of integers
+    # between b1 and b2
+
+    r = race.max_time
+    s = race.record + 1
+
+    b1: float = (-r - sqrt(r**2 - 4 * s)) / 2
+    b2: float = (-r + sqrt(r**2 - 4 * s)) / 2
+
+    return floor(b2) - ceil(b1) + 1
 
 
 def total_winning_strategies(races: list[Race]) -> int:
--- test_day06.py
+++ test_day06.py
@@ -10,17 +10,16 @@ class Day06TestCase(unittest.TestCase):
         with self.subTest(msg="Part 1"):
             races = parse_races(example_data)
             self.assertEqual(total_winning_strategies(races), 288)
-        with self.subTest(msg="Part 1"):
+        with self.subTest(msg="Part 2"):
             races = parse_races_bad_kerning(example_data)
             self.assertEqual(total_winning_strategies(races), 71503)
 
-    @unittest.skip("slow unoptimized solution")
     def test_real_data(self):
         with open("inputs/day06.txt", "r") as f:
             data = f.read()
             with self.subTest(msg="Part 1"):
                 races = parse_races(data)
                 self.assertEqual(total_winning_strategies(races), 449550)
-            with self.subTest(msg="Part 1"):
+            with self.subTest(msg="Part 2"):
                 races = parse_races_bad_kerning(data)
                 self.assertEqual(total_winning_strategies(races), 28360140)
