3ef3b441a3acfcae874b9630df686c9a53783867
karelinas <45606302+karelinas@users.noreply.github.com>
2023-12-09 00:19:39 +0900

    Day 08 part 2 solution

--- day08.py
+++ day08.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
-from itertools import cycle
+from itertools import cycle, takewhile
+from math import lcm
 from sys import stdin
+from typing import Iterable
 import re
 
 
@@ -12,6 +14,7 @@ EdgeDict = dict[str, tuple[str, str]]
 def main() -> None:
     map: Map = Map.from_string(stdin.read())
     print("Part 1:", steps_between(map, start="AAA", stop="ZZZ"))
+    print("Part 2:", ghost_steps(map))
 
 
 @dataclass
@@ -38,7 +41,25 @@ def steps_between(map: Map, start: str, stop: str) -> int:
         start = map.edges[start][instruction]
         if start == stop:
             return count
+
     assert None, "Unreachable"
+
+
+def walker(map: Map, start: str) -> Iterable[str]:
+    for instruction in cycle(map.instructions):
+        yield start
+        start = map.edges[start][instruction]
+
+
+def ghost_steps(map: Map) -> int:
+    start_vertices: list[str] = list(k for k in map.edges.keys() if k.endswith("A"))
+    steps: int = 1
+    for v in start_vertices:
+        trip: int = sum(
+            1 for _ in takewhile(lambda x: not x.endswith("Z"), walker(map, v))
+        )
+        steps = lcm(steps, trip)
+    return steps
 
 
 if __name__ == "__main__":
--- test_day08.py
+++ test_day08.py
@@ -1,12 +1,37 @@
 import unittest
 
-from day08 import Map, steps_between
+from day08 import Map, steps_between, ghost_steps
 
 
-class Day07TestCase(unittest.TestCase):
-    def test_example_data(self):
+class Day08TestCase(unittest.TestCase):
+    def test_example_data_part1(self):
         example_data = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)\n"
 
-        with self.subTest(msg="Part 1"):
-            map = Map.from_string(example_data)
-            self.assertEqual(steps_between(map, start="AAA", stop="ZZZ"), 6)
+        map = Map.from_string(example_data)
+        self.assertEqual(steps_between(map, start="AAA", stop="ZZZ"), 6)
+
+    def test_example_data_part2(self):
+        example_data = (
+            "LR\n"
+            "\n"
+            "11A = (11B, XXX)\n"
+            "11B = (XXX, 11Z)\n"
+            "11Z = (11B, XXX)\n"
+            "22A = (22B, XXX)\n"
+            "22B = (22C, 22C)\n"
+            "22C = (22Z, 22Z)\n"
+            "22Z = (22B, 22B)\n"
+            "XXX = (XXX, XXX)\n"
+        )
+
+        map = Map.from_string(example_data)
+        self.assertEqual(ghost_steps(map), 6)
+
+    def test_real_data(self):
+        with open("inputs/day08.txt", "r") as f:
+            data = f.read()
+            map = Map.from_string(data)
+            with self.subTest(msg="Part 1"):
+                self.assertEqual(steps_between(map, start="AAA", stop="ZZZ"), 19783)
+            with self.subTest(msg="Part 2"):
+                self.assertEqual(ghost_steps(map), 9177460370549)
