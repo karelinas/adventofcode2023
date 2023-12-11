bce93ed7d9c284bdef5574b62f311ebf7069af21
karelinas <45606302+karelinas@users.noreply.github.com>
2023-12-12 00:24:22 +0900

    Day 11 part 2 solution

--- day11.py
+++ day11.py
@@ -7,40 +7,33 @@ from lib import Point, manhattan_distance
 
 
 def main() -> None:
-    grid = parse_grid(stdin.read())
-    grid = expand_universe(grid)
-    print("Part 1:", sum_of_distances(grid))
+    grid = GridDict.from_string(stdin.read())
+
+    part1_grid = expand_universe(grid)
+    print("Part 1:", sum_of_distances(part1_grid))
+
+    part2_grid = expand_universe(grid, hubble_constant=1000000)
+    print("Part 2:", sum_of_distances(part2_grid))
 
 
 class GridDict(dict[Point, str]):
     def __missing__(self, key: Point) -> str:
         return "."
 
-    def __str__(self):
-        stringbuilder: list[str] = []
-
-        max_x = max(p.x for p in self.keys())
-        max_y = max(p.y for p in self.keys())
-        for y in range(max_y + 1):
-            for x in range(max_x + 1):
-                stringbuilder.append(self[Point(x, y)])
-            stringbuilder.append("\n")
-        return "".join(stringbuilder)
-
-
-def parse_grid(data: str) -> GridDict:
-    return GridDict(
-        {
-            Point(x, y): ch
-            for y, line in enumerate(data.split("\n"))
-            if line
-            for x, ch in enumerate(line)
-            if ch == "#"
-        }
-    )
+    @staticmethod
+    def from_string(data: str) -> "GridDict":
+        return GridDict(
+            {
+                Point(x, y): ch
+                for y, line in enumerate(data.split("\n"))
+                if line
+                for x, ch in enumerate(line)
+                if ch == "#"
+            }
+        )
 
 
-def expand_universe(grid: GridDict) -> GridDict:
+def expand_universe(grid: GridDict, hubble_constant=2) -> GridDict:
     def expand_axis(grid: GridDict, axis: str) -> GridDict:
         max_coord = max(getattr(p, axis) for p in grid.keys())
         expansion: int = 0
@@ -48,7 +41,7 @@ def expand_universe(grid: GridDict) -> GridDict:
         for coord in range(max_coord + 1):
             points = [p for p in grid.keys() if getattr(p, axis) == coord]
             if not points:
-                expansion += 1
+                expansion += hubble_constant - 1
             else:
                 new_grid.update(
                     {
--- test_day11.py
+++ test_day11.py
@@ -1,9 +1,9 @@
 import unittest
 
-from day11 import parse_grid, sum_of_distances, expand_universe
+from day11 import GridDict, sum_of_distances, expand_universe
 
 
-class Day10TestCase(unittest.TestCase):
+class Day11TestCase(unittest.TestCase):
     def test_example_data_part1(self):
         example_data = (
             "...#......\n"
@@ -17,6 +17,25 @@ class Day10TestCase(unittest.TestCase):
             ".......#..\n"
             "#...#.....\n"
         )
-        grid = parse_grid(example_data)
-        grid = expand_universe(grid)
-        self.assertEqual(sum_of_distances(grid), 374)
+        grid = GridDict.from_string(example_data)
+
+        with self.subTest(msg="Part 1"):
+            grid_part1 = expand_universe(grid)
+            self.assertEqual(sum_of_distances(grid_part1), 374)
+        with self.subTest(msg="Part 2", hubble_constant=10):
+            grid_part2_10 = expand_universe(grid, hubble_constant=10)
+            self.assertEqual(sum_of_distances(grid_part2_10), 1030)
+        with self.subTest(msg="Part 2", hubble_constant=100):
+            grid_part2_100 = expand_universe(grid, hubble_constant=100)
+            self.assertEqual(sum_of_distances(grid_part2_100), 8410)
+
+    def test_real_data(self):
+        with open("inputs/day11.txt", "r") as f:
+            data = f.read()
+            grid = GridDict.from_string(data)
+            with self.subTest(msg="Part 1"):
+                grid_part1 = expand_universe(grid)
+                self.assertEqual(sum_of_distances(grid_part1), 9609130)
+            with self.subTest(msg="Part 2"):
+                grid_part2 = expand_universe(grid, hubble_constant=1000000)
+                self.assertEqual(sum_of_distances(grid_part2), 702152204842)
