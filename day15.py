aa937f1b6b7dea5e4cb9dbf042646638276fe06e
karelinas <45606302+karelinas@users.noreply.github.com>
2023-12-15 17:34:24 +0900

    Day 15 part 2 solution

--- day15.py
+++ day15.py
@@ -1,11 +1,18 @@
+from collections import defaultdict
+from dataclasses import dataclass
 from functools import reduce
 from sys import stdin
-from typing import Callable
+from typing import Callable, Mapping, MutableMapping
 
 
 def main() -> None:
-    data: str = stdin.read()
-    print("Part 1:", sum_of_hashes(data))
+    steps = parse_steps(stdin.read())
+    print("Part 1:", sum_of_hashes(steps))
+    print("Part 2:", total_focusing_power(steps))
+
+
+def parse_steps(data: str) -> list[str]:
+    return [piece.strip() for piece in data.split(",")]
 
 
 def reindeer_hash(data: str) -> int:
@@ -13,8 +20,60 @@ def reindeer_hash(data: str) -> int:
     return reduce(hash_fn, data, 0)
 
 
-def sum_of_hashes(data: str) -> int:
-    return sum(reindeer_hash(piece.strip()) for piece in data.split(","))
+def sum_of_hashes(steps: list[str]) -> int:
+    return sum(reindeer_hash(s.strip()) for s in steps)
+
+
+@dataclass
+class Lens:
+    label: str
+    focal_length: int
+
+    @staticmethod
+    def from_string(data: str) -> "Lens":
+        l, d = data.split("=")
+        return Lens(label=l, focal_length=int(d))
+
+
+def build_boxes(steps: list[str]) -> Mapping[int, list[Lens]]:
+    boxes: MutableMapping[int, list[Lens]] = defaultdict(list[Lens])
+    for s in steps:
+        if "=" in s:
+            # insert or replace lens
+            new_lens = Lens.from_string(s)
+            box_number = reindeer_hash(new_lens.label)
+
+            if any(new_lens.label == lens.label for lens in boxes[box_number]):
+                # replace existing lens
+                boxes[box_number] = [
+                    lens if new_lens.label != lens.label else new_lens
+                    for lens in boxes[box_number]
+                ]
+            else:
+                # insert new lens
+                boxes[box_number].append(new_lens)
+
+        elif "-" in s:
+            # remove lens if it exists
+            label, _ = s.split("-")
+            box_number = reindeer_hash(label)
+            boxes[box_number] = [
+                lens for lens in boxes[box_number] if lens.label != label
+            ]
+
+        else:
+            raise RuntimeError(f"Invalid step '{s}'")
+
+    return boxes
+
+
+def total_focusing_power(steps: list[str]) -> int:
+    boxes = build_boxes(steps)
+    return sum(
+        (box_number + 1) * (slot_number) * lens.focal_length
+        for box_number, lenses in boxes.items()
+        for slot_number, lens in enumerate(lenses, start=1)
+    )
 
 
 if __name__ == "__main__":
--- test_day15.py
+++ test_day15.py
@@ -1,10 +1,21 @@
 import unittest
 
-from day15 import sum_of_hashes
+from day15 import parse_steps, sum_of_hashes, total_focusing_power
 
 
 class Day15TestCase(unittest.TestCase):
     def test_example_data(self):
-        example_data = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+        steps = parse_steps("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
         with self.subTest(msg="Part 1"):
-            self.assertEqual(sum_of_hashes(example_data), 1320)
+            self.assertEqual(sum_of_hashes(steps), 1320)
+        with self.subTest(msg="Part 2"):
+            self.assertEqual(total_focusing_power(steps), 145)
+
+    def test_real_data(self):
+        with open("inputs/day15.txt", "r") as f:
+            data = f.read()
+            steps = parse_steps(data)
+            with self.subTest(msg="Part 1"):
+                self.assertEqual(sum_of_hashes(steps), 498538)
+            with self.subTest(msg="Part 2"):
+                self.assertEqual(total_focusing_power(steps), 286278)
