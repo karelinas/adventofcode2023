ac3fc0f11b56b76deffd955bf0b640f48d9273cf
karelinas <45606302+karelinas@users.noreply.github.com>
2023-12-17 00:15:02 +0900

    Day 16 part 2 solution

--- day16.py
+++ day16.py
@@ -9,6 +9,7 @@ def main() -> None:
     grid = Grid.from_string(stdin.read())
     start_beam = Beam(pos=Point(-1, 0), v=Point(1, 0))
     print("Part 1:", count_energized_tiles(grid, start_beam))
+    print("Part 2:", most_energized_configuration(grid))
 
 
 class GridDict(dict[Point, str]):
@@ -50,7 +51,7 @@ class Beam:
     v: Point
 
 
-def transform(mat: list[list[int]], vec: Point) -> Point:
+def matrix_transform(mat: list[list[int]], vec: Point) -> Point:
     vec_: list[int] = [vec.x, vec.y]
     return Point(*[sum(a * b for a, b in zip(row, vec_)) for row in mat])
 
@@ -93,7 +94,7 @@ def left_mirror(beam_v: Point) -> list[Point]:
     #
     # |0 1| |1| = |0|      |0 1| |-1| = | 0|    |0 1| |0| = |1|      |0 1| | 0| = |-1|
     # |1 0| |0|   |1|      |1 0| | 0|   |-1|    |1 0| |1|   |0|      |1 0| |-1|   | 0|
-    return [transform([[0, 1], [1, 0]], beam_v)]
+    return [matrix_transform([[0, 1], [1, 0]], beam_v)]
 
 
 def right_mirror(beam_v: Point) -> list[Point]:
@@ -108,7 +109,7 @@ def right_mirror(beam_v: Point) -> list[Point]:
     #
     # | 0 -1| |1| = | 0|   | 0 -1| |-1| = |0|   | 0 -1| |0| = |-1|   | 0 -1| | 0| = |1|
     # |-1  0| |0|   |-1|   |-1  0| | 0|   |1|   |-1  0| |1|   | 0|   |-1  0| |-1|   |0|
-    return [transform([[0, -1], [-1, 0]], beam_v)]
+    return [matrix_transform([[0, -1], [-1, 0]], beam_v)]
 
 
 def empty_space(beam_v: Point) -> list[Point]:
@@ -126,23 +127,39 @@ TRANSFORMATIONS: dict[str, Callable[[Point], list[Point]]] = {
 
 def count_energized_tiles(grid: Grid, beam: Beam) -> int:
     seen: set[Beam] = set()
-
     beams: list[Beam] = [beam]
-    while beams:
-        current: Beam = beams.pop()
-        current = Beam(pos=current.pos + current.v, v=current.v)
 
-        if not grid.in_bounds(current.pos) or current in seen:
+    while beams:
+        past_beam: Beam = beams.pop()
+        beam = Beam(pos=past_beam.pos + past_beam.v, v=past_beam.v)
+
+        if not grid.in_bounds(beam.pos) or beam in seen:
+            # The beam is either out of the grid and continues out to infinity, or
+            # it is stuck in an infinite loop. In either case, we have already seen
+            # all the tiles that it will energize, so it's safe to stop simulating
+            # the beam any further.
             continue
 
-        seen.add(current)
-
+        seen.add(beam)
+        tile: str = grid.grid[beam.pos]
         beams.extend(
-            Beam(pos=current.pos, v=new_v)
-            for new_v in TRANSFORMATIONS[grid.grid[current.pos]](current.v)
+            Beam(pos=beam.pos, v=new_v) for new_v in TRANSFORMATIONS[tile](beam.v)
         )
 
     return len(set(b.pos for b in seen))
+
+
+def most_energized_configuration(grid: Grid) -> int:
+    start_beams: list[Beam] = []
+
+    for x in range(grid.width):
+        start_beams.append(Beam(pos=Point(x, -1), v=Point.south()))
+        start_beams.append(Beam(pos=Point(x, grid.height), v=Point.north()))
+    for y in range(grid.height):
+        start_beams.append(Beam(pos=Point(-1, y), v=Point.east()))
+        start_beams.append(Beam(pos=Point(grid.width, y), v=Point.west()))
+
+    return max(count_energized_tiles(grid, beam) for beam in start_beams)
 
 
 if __name__ == "__main__":
--- test_day16.py
+++ test_day16.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day16 import Beam, Grid, count_energized_tiles
+from day16 import Beam, Grid, count_energized_tiles, most_energized_configuration
 from lib import Point
 
 
@@ -21,5 +21,23 @@ class Day16TestCase(unittest.TestCase):
             "..//.|....\n"
         )
         grid = Grid.from_string(example_data)
-        start_beam = Beam(pos=Point(-1, 0), v=Point(1, 0))
-        self.assertEqual(count_energized_tiles(grid, start_beam), 46)
+
+        with self.subTest(msg="Part 1"):
+            start_beam = Beam(pos=Point(-1, 0), v=Point(1, 0))
+            self.assertEqual(count_energized_tiles(grid, start_beam), 46)
+
+        with self.subTest(msg="Part 2"):
+            self.assertEqual(most_energized_configuration(grid), 51)
+
+    @unittest.skip("Too slow; takes 10 seconds")
+    def test_real_data(self):
+        with open("inputs/day16.txt", "r") as f:
+            data = f.read()
+            grid = Grid.from_string(data)
+
+            with self.subTest(msg="Part 1"):
+                start_beam = Beam(pos=Point(-1, 0), v=Point(1, 0))
+                self.assertEqual(count_energized_tiles(grid, start_beam), 7111)
+
+            with self.subTest(msg="Part 2"):
+                self.assertEqual(most_energized_configuration(grid), 7831)
